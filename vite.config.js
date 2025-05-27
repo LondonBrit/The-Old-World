c9e0d435464cfbcb3d9b3a8ad317b3862b2d927f
LondonBrit <53127590+LondonBrit@users.noreply.github.com>
2025-05-27 10:04:39 +0000

    updated base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/THE-OLD-WORLD/', // Make absolutely sure this line is correct and present!
+  base: '', // Make absolutely sure this line is correct and present!
 })
